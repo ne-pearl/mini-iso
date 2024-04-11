fc30cb3dd3e9d68bacaacb2affee6199750f5268
Jon Pearce <jonpea@gmail.com>
2024-04-11 09:18:59 -0400

    Updated layout: Column to Row.

--- mini_iso/auction.py
+++ mini_iso/auction.py
@@ -249,7 +249,7 @@ class Bidder(pn.viewable.Viewer):
                             ),
                         ),
                         pn.Card(
-                            pn.Column(
+                            pn.Row(
                                 pn.Card(
                                     pn.widgets.Tabulator.from_param(
                                         self.param.offers_committed,
