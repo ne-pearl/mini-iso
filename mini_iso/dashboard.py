edec0f86f33b7a03016bbc450b932f85688b7404
Jon Pearce <jonpea@gmail.com>
2024-04-21 10:24:48 -0400

    Changed "Isolated" to "Isolated Zone"

--- mini_iso/dashboard.py
+++ mini_iso/dashboard.py
@@ -817,7 +817,7 @@ class LmpDashboard(pm.Parameterized):
                     pn.Row(
                         labeled(pn.pane.Vega(offer_stacks_chart_zonal), label="Actual"),
                         labeled(
-                            pn.pane.Vega(offer_stacks_chart_ideal), label="Isolated"
+                            pn.pane.Vega(offer_stacks_chart_ideal), label="Isolated Zone"
                         ),
                     ),
                 ),
