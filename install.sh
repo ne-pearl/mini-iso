e2def7853f6142943275c4d756d4be748c1f8062
Jon Pearce <jonpea@gmail.com>
2024-05-09 22:12:47 -0400

    Added datasets folder to pyinstaller single-file executable

--- install.sh
+++ install.sh
@@ -3,13 +3,17 @@
 # Use --onedir or --onefile
 pyinstaller --onefile \
     mini_iso/app.py \
+    --name mini_iso \
     --hiddenimport pydantic.deprecated.decorator \
-    --add-data ./gurobi.lic:gurobipy/.libs/
+    --add-data ./gurobi.lic:gurobipy/.libs/ \
+    --add-data ./mini_iso/datasets/:datasets/
 
-# Additional text file not picked up by pyinstaller
-cp gurobi.lic dist/app/_internal/gurobipy/.libs/.
-
+echo ""
 echo "To run, use: ./dist/app/app absolute/path/to/data.json"
-echo "Example:"
-echo "./dist/app/app \\"
-echo "\$(realpath mini_iso/datasets/mini_new_england/mini_new_england.json)"
+echo "Examples:"
+echo "$ dist/mini_iso datasets/one_zone/one_zone.json"
+echo "$ dist/mini_iso datasets/one_zones/case1.json"
+echo "$ dist/mini_iso datasets/one_zones/case2.json"
+echo "$ dist/mini_iso datasets/one_zones/case3.json"
+echo "$ dist/mini_iso datasets/mini_new_england/mini_new_england.json"
+echo ""
