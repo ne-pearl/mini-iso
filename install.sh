b5414a9875909c09f9d94a7a54691b1e63c9bff4
Jon Pearce <jonpea@gmail.com>
2024-05-21 10:32:42 -0400

    Updated appliation name in install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 # NB: The two locations of gurobi.lic are for Windows and Mac/Linux
 pyinstaller --onefile \
     mini_iso/app.py \
-    --name mini_iso \
+    --name mini-iso \
     --hiddenimport pydantic.deprecated.decorator \
     --add-data ./gurobi.lic:gurobipy/ \
     --add-data ./gurobi.lic:gurobipy/.libs/ \
@@ -13,7 +13,7 @@ pyinstaller --onefile \
 echo ""
 echo "To run, use: ./dist/app/app absolute/path/to/data.json"
 echo "Examples:"
-echo "$ dist/mini_iso three-zones-case1  # 1-4"
-echo "$ dist/mini_iso mini-new-england-uniform"
-echo "$ dist/mini_iso mini-new-england-nonuniform"
+echo "$ dist/mini-iso three-nodes"
+echo "$ dist/mini-iso mini-new-england-uni"
+echo "$ dist/mini-iso mini-new-england-multi"
 echo ""
