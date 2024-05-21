65f4bcd00114c6af2ba16aa58a266edcf2c67631
Jon Pearce <jonpea@gmail.com>
2024-05-21 15:24:37 -0400

    Added extra --hiddenimport to fix pyinstaller issue on Windows

--- install.sh
+++ install.sh
@@ -2,13 +2,8 @@
 
 # Use --onedir or --onefile
 # NB: The two locations of gurobi.lic are for Windows and Mac/Linux
-pyinstaller --onefile \
-    mini_iso/app.py \
-    --name mini-iso \
-    --hiddenimport pydantic.deprecated.decorator \
-    --add-data ./gurobi.lic:gurobipy/ \
-    --add-data ./gurobi.lic:gurobipy/.libs/ \
-    --add-data ./mini_iso/datasets/:./
+# NB: `pkg_resources.extern` is missing on Windows (May 2024)
+pyinstaller --onefile mini_iso/app.py --name mini-iso --hiddenimport pydantic.deprecated.decorator --hiddenimport pkg_resources.extern --add-data ./gurobi.lic:gurobipy/ --add-data ./gurobi.lic:gurobipy/.libs/ --add-data ./mini_iso/datasets/:./
 
 echo ""
 echo "To run, use: ./dist/app/app absolute/path/to/data.json"
